d78b25fdab9e8e65db7c674af8e7ec1f2fdb7831
万里涛 <wanlitao_3055601@outlook.com>
2025-03-03 17:40:22 +0800

    add config hook samples

--- playground/electron-nitro.config.ts
+++ playground/electron-nitro.config.ts
@@ -1,3 +1,17 @@
 import { defineElectronNitroConfig } from "@electron-nitro/core/configurer";
 
-export default defineElectronNitroConfig({});
+export default defineElectronNitroConfig({
+  hooks: {
+    "nitro:init": (nitro) => {
+      console.log("nitro options entry:", nitro.options.entry);
+    },
+    "nitro:dev:listen": async (nitro, listener) => {
+      const listenUrls = await listener.getURLs();
+
+      console.log(
+        "nitro dev server listening on:",
+        listenUrls.map((x) => `${x.type}: ${x.url}`).join(", ")
+      );
+    },
+  },
+});
